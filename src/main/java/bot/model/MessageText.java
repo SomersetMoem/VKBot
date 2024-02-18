ea9f66eb24b8671ccb49542e6d4b2457c70ef757
SommersetMoem <nnekit10@mail.ru>
2024-02-18 15:57:47 +0300

    add DataInitializer

--- src/main/java/bot/model/MessageText.java
+++ src/main/java/bot/model/MessageText.java
@@ -17,19 +17,19 @@ public class MessageText {
     public static MessageText messageText = new MessageText();
     private final static Logger LOG = Logger.getLogger(DbUtils.class);
 
-    private String welcomeMessage = "Добро пожаловать в студию красоты SlyFox! \uD83C\uDF1F\n" +
+    private final String WELCOME_MESSAGE = "Добро пожаловать в студию красоты SlyFox! \uD83C\uDF1F\n" +
             "Я готов помочь вам стать ещё красивее! \uD83D\uDC85\n" +
             "Пожалуйста, выберете соответствующий пункт меню!";
-    private String monthSelectMessage = "Выберите месяц для записи:)";
-    private String daySelectMessage = "Выберите день для записи:)";
-    private String timeSelectMessage = "Выберите время для записи:)";
+    private final String MONTH_SELECT_MESSAGE = "Выберите месяц для записи:)";
+    private final String DAY_SELECT_MESSAGE = "Выберите день для записи:)";
+    private final String TIME_SELECT_MESSAGE = "Выберите время для записи:)";
 
 
     public static Map<String, String> getMapMessageText() {
         Map<String, String> payloadToMethodMap = new HashMap<>();
-        payloadToMethodMap.put("bookProcedure", messageText.monthSelectMessage);
-        payloadToMethodMap.put("selectMonthMenu", messageText.daySelectMessage);
-        payloadToMethodMap.put("selectDayMenu", messageText.timeSelectMessage);
+        payloadToMethodMap.put("bookProcedure", messageText.MONTH_SELECT_MESSAGE);
+        payloadToMethodMap.put("selectMonthMenu", messageText.DAY_SELECT_MESSAGE);
+        payloadToMethodMap.put("selectDayMenu", messageText.TIME_SELECT_MESSAGE);
         payloadToMethodMap.put("checkMyProcedure", "В разработке");
         payloadToMethodMap.put("priceList", dataInitPriceList());
         payloadToMethodMap.put("canselMyProcedure", "В разработке");
@@ -37,6 +37,11 @@ public class MessageText {
     }
 
     public static String dataInitPriceList() {
-        LOG.
+        StringBuilder priceList = new StringBuilder();
+        priceList.append("🌟 **Прайс-лист на наращивание ресниц** 🌟\n\n");
+        priceList.append("1. Наращивание ресниц классика - 700 рублей\n");
+        priceList.append("2. Наращивание ресниц 2D - 999 рублей\n");
+        priceList.append("3. Наращивание уголков глаз - 500 рублей\n");
+        return priceList.toString();
     }
 }
